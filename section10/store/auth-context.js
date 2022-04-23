47cdeba565deacb6dba88f69e72b1ff5b61ff365
Ali Hazimeh <ahazimeh@globalistic.tech>
2022-04-23 15:29:36 +0300

    auth context with state management

--- section10/store/auth-context.js
+++ section10/store/auth-context.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const AuthContext = React.createContext({
+  //typically you need to set values for ide completition
   isLoggedIn: false,
+  onLogout: () => {},
 });
 
 export default AuthContext;
